e8841e59f47803f07b34b33b301b5509cccd6a39
Pavel <pavel.de-wavrechin@epitech.eu>
2024-10-29 17:20:58 +0100

    Add flags system to specifiers c and s + try fixing my_put_float

--- src/specifiers/print_char.c
+++ src/specifiers/print_char.c
@@ -11,8 +11,14 @@ void print_char(va_list *list, int *nb_output_char,
     int *index, const char *format)
 {
     char character = va_arg(*list, int);
+    int flag_minus = flag_minus_finder(format, index);
 
     my_putchar(character);
     *nb_output_char += 1;
-    *index += 1;
+    *index += get_next_char(format, index);
+    if (flag_minus > 0)
+        for (int i = 0; i < flag_minus - 1; i++) {
+            my_putchar(' ');
+            *nb_output_char += 1;
+        }
 }
